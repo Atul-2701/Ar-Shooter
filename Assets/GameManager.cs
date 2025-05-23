2c85321576127cb430ae7e73a63991a207d2a258
Atul-2701 <136069734+Atul-2701@users.noreply.github.com>
2025-05-23 19:40:03 +0530

    Fix bugs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,6 +13,7 @@ public class GameManager : MonoBehaviour
     public Scrollbar xAxis;
     public Scrollbar yAxis;
     public Scrollbar Distance;
+    public Vector3 position { get; private set; }
 
     [SerializeField] Text xValue;
     [SerializeField] Text yValue;
@@ -27,9 +28,10 @@ public class GameManager : MonoBehaviour
         xAxis.value = 0.5f;
         yAxis.value = 0.5f;
         Distance.value = 0.4f;
-        xAxis.onValueChanged.AddListener(delegate { UpdateValue(pos); });
-        yAxis.onValueChanged.AddListener(delegate { UpdateValue(pos); });
-        Distance.onValueChanged.AddListener(delegate { UpdateValue(pos); });
+        UpdateValue();
+        xAxis.onValueChanged.AddListener(delegate { UpdateValue(); });
+        yAxis.onValueChanged.AddListener(delegate { UpdateValue(); });
+        Distance.onValueChanged.AddListener(delegate { UpdateValue(); });
         Instantiate(target, Boundary(xAxis.value, xAxis.value, Distance.value), Quaternion.identity);
     }
 
@@ -50,25 +52,28 @@ public class GameManager : MonoBehaviour
         if (menuPanel.activeInHierarchy)
         {
             menuPanel.SetActive(false);
+            Time.timeScale = 1f;
         }
         else
         {
             menuPanel.SetActive(true);
+            Time.timeScale = 0f;
         }
     }
 
-    public void UpdateValue(Vector3 position)
+    public void UpdateValue()
     {
         position = Boundary(xAxis.value, xAxis.value, Distance.value);
-        xValue.text = (xAxis.value * 10f).ToString();
-        yValue.text = (yAxis.value * 10f).ToString();
-        distanceValue.text = (Distance.value * 30f).ToString();
-
+        xValue.text = ((int)(xAxis.value * 10f)).ToString();
+        yValue.text = ((int)(yAxis.value * 10f)).ToString();
+        distanceValue.text = ((int)(Distance.value * 15f)).ToString();
+        Debug.Log(position + "hdfxcjdsfjhdsjkfh");
+        return;
     }
 
     public Vector3 Boundary(float xAxis, float yAxis, float Distance)
     {
-        Vector3 SpawnPoint = new Vector3(Random.Range(xAxis, -xAxis) * 10f, Random.Range(yAxis, -yAxis) * 10f, Distance * 30f);
+        Vector3 SpawnPoint = new Vector3(Random.Range(xAxis, -xAxis) * 10f, Random.Range(yAxis, -yAxis) * 10f, Distance * 15f);
         return SpawnPoint;
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,6 +1,7 @@
 using BigRookGames.Weapons;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,13 +9,15 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] Text scoreText;
-    Vector3 postion;
+
+    Animator animatorController;
 
     int score = 0;
 
     private void Start()
     {
         scoreText.text = "Score: " + score.ToString();
+        animatorController = GetComponentInChildren<Animator>();
     }
     private void Update()
     {
@@ -22,22 +25,28 @@ public class Player : MonoBehaviour
         {
             ShootTarget();
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameManager.GetInstance().CloseAndOpenMenu();
+        }
     }
 
     public void ShootTarget()
     {
+        animatorController.SetTrigger("CanShoot");
         GunfireController.GetInstance().FireWeapon();
-        Vector3 SpawnPoint = new Vector3(Random.Range(5f, -5f), Random.Range(5f, -5f), 15f);
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, transform.forward, out hit))
         {
             if (hit.transform.CompareTag("Target"))
             {
-                GameManager.instance.UpdateValue(postion);
+                GameManager.GetInstance().UpdateValue();
+                Debug.Log(GameManager.GetInstance().position);
                 score++;
                 scoreText.text = "Score: " + score.ToString();
-                hit.transform.position = postion;
+                hit.transform.position = GameManager.GetInstance().position;
                 hit.transform.rotation = Quaternion.identity;
             }
         }
